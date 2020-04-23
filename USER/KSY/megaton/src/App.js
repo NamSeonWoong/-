184360471037a06c9e3e0581dd5d926fa30884d9
vxda7 <vxda77@gmail.com>
2020-04-23 17:06:07 +0900

    [+]front mainpage function

--- USER/KSY/megaton/src/App.js
+++ USER/KSY/megaton/src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Button>홈</Button>
         </Link>
       
-        <Link to="/about">
+        <Link to={"/about"}>
           <Button>소개</Button>
         </Link>
       </div>
@@ -22,6 +22,43 @@ function App() {
       <Route path="/" component={Home} exact={true}/>
       <Route path="/about" component={About} exact={true}/>
       <Route path="/search" component={Search} exact={true}/>
+
+
+
+      <footer
+        style={{
+          backgroundColor: "#eceff1",
+          color: "#757575",
+          marginTop: "2%",
+          paddingLeft: "10%",
+          paddingRight: "10%",
+          paddingTop: "2%",
+          paddingBottom: "2%"
+        }}
+      >
+        <hr style={{ marginBottom: "2%" }} />
+        {/* <div
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        > */}
+          <div>
+            copyright® Cat Ltd. All rights Reserved.
+            <br />
+            대표 : CAT
+            <br />
+            대표연락처 : vxda7@naver.com
+            <br />
+          </div>
+          <div>
+            {/* <img src={javalogo} alt="javalogo" style={{ height: "50px" }} />
+            <img src={springlogo} alt="springlogo" style={{ height: "50px" }} />
+            <img src={reactlogo} alt="reactlogo" style={{ height: "50px" }} />
+            <img src={dockerlogo} alt="dockerlogo" style={{ height: "50px" }} /> */}
+          </div>
+        {/* </div> */}
+      </footer>
     </div>
   );
 }
