53cb06d2f7a9ae4ffa59d552c1613e77a3b51167
vxda7 <vxda77@gmail.com>
2020-04-24 15:02:40 +0900

    [+]more pagination

--- USER/KSY/megaton/src/componenets/Board.js
+++ USER/KSY/megaton/src/componenets/Board.js
@@ -1,52 +1,32 @@
 import React from 'react'
 import {Table} from 'react-bootstrap'
 
-const Board = ({data, setData}) => {
-    const axiosdata = [
-        {
-            "articleid": "휴대폰 판매/매입",
-            "title": "갤럭시 s10e 팝니다.",
-            "user": "바삭한피자",
-            "date": "2020.04.23",
-            "url": "https://cafe.naver.com/joonggonara.cafe?iframe_url=/ArticleList.nhn%3Fsearch.clubid=10050146%26search.boardtype=L%26viewType=pc"
-        },
-        {
-            "articleid": "휴대폰 판매/매입",
-            "title": "갤럭시 s10e 팝니다.",
-            "user": "바삭한피자",
-            "date": "2020.04.23",
-            "url": "https://cafe.naver.com/joonggonara.cafe?iframe_url=/ArticleList.nhn%3Fsearch.clubid=10050146%26search.boardtype=L%26viewType=pc"
-        },
-        {
-            "articleid": "휴대폰 판매/매입",
-            "title": "갤럭시 s10e 팝니다.",
-            "user": "바삭한피자",
-            "date": "2020.04.23",
-            "url": "https://cafe.naver.com/joonggonara.cafe?iframe_url=/ArticleList.nhn%3Fsearch.clubid=10050146%26search.boardtype=L%26viewType=pc"
-        },
-    ]
+
+const Board = ({data, setData, realdata}) => {
     return (
-        <Table responsive>
-            <thead>
-            <tr>
-                <th className="text-center">카테고리</th>
-                <th className="text-center">제목</th>
-                <th className="text-center">작성자</th>
-                <th className="text-center">작성일</th>
-            </tr>
-            </thead>
-            <tbody>
-                {axiosdata.map(data => (
-                    <tr>
-                        <td className="text-center">{data.articleid}</td>
-                        <td><a href={data.url}>{data.title}</a></td>    
-                        <td className="text-center">{data.user}</td>
-                        <td className="text-center">{data.date}</td>
-                    </tr>
-                ))}
-                
-            </tbody>
-        </Table>
+        <>
+            <Table responsive>
+                <thead>
+                <tr>
+                    <th className="text-center">카테고리</th>
+                    <th className="text-center">제목</th>
+                    <th className="text-center">작성자</th>
+                    <th className="text-center">작성일</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {realdata.map(data => (
+                        <tr>
+                            <td className="text-center">{data.category}</td>
+                            <td><a href={data.url}>{data.title}</a></td>    
+                            <td className="text-center">{data.user}</td>
+                            <td className="text-center">{data.date}</td>
+                        </tr>
+                    ))}
+                    
+                </tbody>
+            </Table>
+        </>
     )
 }
 
--- USER/KSY/megaton/src/pages/Home.js
+++ USER/KSY/megaton/src/pages/Home.js
@@ -2,11 +2,42 @@ import React from 'react';
 import {InputGroup, Button, FormControl, Container, Row} from 'react-bootstrap';
 import catswim from "../images/catswim2.png"
 import Board from "../componenets/Board"
-// import axios from 'axios';
+import axios from 'axios';
+
+// function simulateNetworkRequest() {
+//     return new Promise((resolve) => setTimeout(resolve, 2000));
+//   }
+  
+// function LoadingButton() {
+//     const [isLoading, setLoading] = React.useState(false);
+
+//     React.useEffect(() => {
+//         if (isLoading) {
+//         simulateNetworkRequest().then(() => {
+//             setLoading(false);
+//         });
+//         }
+//     }, [isLoading]);
+
+//     const handleClick = () => setLoading(true);
+
+//     return (
+//         <Button
+//         variant="primary"
+//         disabled={isLoading}
+//         onClick={!isLoading ? handleClick : null}
+//         >
+//         {isLoading ? 'Loading…' : 'Click to load'}
+//         </Button>
+//     );
+// }
+
 
 const Home = ({history}) => {
+    const [realdata, setRealdata] = React.useState([]);
     const [data, setData] = React.useState("");
     const [inputdata, setInputdata] = React.useState("")
+    let page = 1;
     const handleChange = (e) => {
         setInputdata(e.target.value)
     }
@@ -16,14 +47,35 @@ const Home = ({history}) => {
     }
     const Search = () => {
         setData(inputdata);
-        setInputdata("");
-        // history.push("/about");
+        // setInputdata("");
+        axios.get(`http://i02c102.p.ssafy.io:5000/search/${inputdata}/${page}`)
+        .then(
+            (res)=>{
+                console.log(res.data)
+                setRealdata(res.data)
+            }
+        ).catch((e)=>{
+            console.log(e)
+        })
     }
     const handleKeyPress = (e) => {
         if (e.key === "Enter"){
             Search()
         }
     }
+
+    const Morepage = () => {
+        axios.get(`http://i02c102.p.ssafy.io:5000/search/${inputdata}/${page + 1}`)
+            .then((res)=>{
+                console.log(res.data)
+                setRealdata(realdata.concat(res.data))
+                page += 1;
+            })
+            .catch((e)=>{
+                console.log(e)
+            })
+    }
+
 
     return (
         <div>
@@ -46,9 +98,13 @@ const Home = ({history}) => {
                         </InputGroup.Append>
                     </InputGroup>
                 </Row>
-                <Row>
+                <Row className="justify-content-center">
                     {data ? (
-                        <Board data={data} setData={setData}/>
+                        <>
+                            <Board data={data} setData={setData} realdata={realdata}/>
+                            {/* {LoadingButton()} */}
+                            <Button onClick={Morepage}>더보기</Button>
+                        </>
                     ) : (
                         <></>
                     )}
