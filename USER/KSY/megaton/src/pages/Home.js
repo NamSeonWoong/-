9b7f03ff588a92de89093e6c50babf63e3478330
vxda7 <vxda77@gmail.com>
2020-04-29 10:12:55 +0900

    [+]filter, search Loading

--- USER/KSY/megaton/src/pages/Home.js
+++ USER/KSY/megaton/src/pages/Home.js
@@ -1,45 +1,21 @@
-import React from 'react';
-import {InputGroup, Button, FormControl, Container, Row} from 'react-bootstrap';
+import React from 'react'
+import {InputGroup, Button, FormControl, Container, Row, Form} from 'react-bootstrap'
 import catswim from "../images/catswim2.png"
 import Board from "../componenets/Board"
-import axios from 'axios';
-
-// function simulateNetworkRequest() {
-//     return new Promise((resolve) => setTimeout(resolve, 2000));
-//   }
-  
-// function LoadingButton() {
-//     const [isLoading, setLoading] = React.useState(false);
-
-//     React.useEffect(() => {
-//         if (isLoading) {
-//         simulateNetworkRequest().then(() => {
-//             setLoading(false);
-//         });
-//         }
-//     }, [isLoading]);
-
-//     const handleClick = () => setLoading(true);
-
-//     return (
-//         <Button
-//         variant="primary"
-//         disabled={isLoading}
-//         onClick={!isLoading ? handleClick : null}
-//         >
-//         {isLoading ? 'Loading…' : 'Click to load'}
-//         </Button>
-//     );
-// }
+import axios from 'axios'
 
 
-const Home = ({history}) => {
+const Home = ({history, props, setSearchloading}) => {
     const [realdata, setRealdata] = React.useState([]);
     const [nextdata, setNextdata] = React.useState([]); // 미리 다음페이지 로딩
     const [data, setData] = React.useState("");
     const [inputdata, setInputdata] = React.useState("")
     const [page, setPage] = React.useState(1)
     const [loading, setLoading] = React.useState(false)
+    const [filterState, setFilterState] = React.useState("1");
+    const handleFilter = (e) => {
+        setFilterState(e.target.value)
+    }
     const handleChange = (e) => {
         setInputdata(e.target.value)
     }
@@ -50,12 +26,13 @@ const Home = ({history}) => {
     const Search = () => {
         setLoading(true)
         setData(inputdata);
-        // setInputdata("");
+        setSearchloading(true)
         axios.get(`http://i02c102.p.ssafy.io:5000/search/${inputdata}/${page}`)
         .then(
             (res)=>{
                 console.log(res.data)
                 setRealdata(res.data)
+                setSearchloading(false)
             }
         ).catch((e)=>{
             console.log(e)
@@ -116,12 +93,21 @@ const Home = ({history}) => {
                         <Button variant="outline-secondary" onClick={Search}>검색</Button>
                         </InputGroup.Append>
                     </InputGroup>
+                    <Form>
+                        <Form.Group controlId="exampleForm.SelectCustom">
+                            <Form.Label></Form.Label>
+                            <Form.Control as="select" custom value={filterState} onChange={handleFilter}>
+                                <option value="1">모두</option>
+                                <option value="2">일반</option>
+                                <option value="3">의심</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
                 </Row>
                 <Row className="justify-content-center">
                     {data ? (
                         <>
-                            <Board realdata={realdata}/>
-                            {/* {LoadingButton()} */}
+                            <Board realdata={realdata} filterState={filterState}/>
                             {loading ? (
                                 <Button disabled>로딩중...</Button>
                             ):(
